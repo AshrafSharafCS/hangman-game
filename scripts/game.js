e521ff8c153a03dcecd858a37f16eb50727008a4
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-02 18:37:07 +0200

    fixing some bugs in the alreadyGuessed function

--- scripts/game.js
+++ scripts/game.js
@@ -5,7 +5,7 @@ const randomNum = Math.floor(Math.random() * words.length);
 const word = words[randomNum];
 
 let letters = word.split("");
-const alreadyGuessedArray = [];
+let alreadyGuessedArray = [];
 let correctLetter;
 let guess;
 
@@ -20,33 +20,36 @@ function checkLetter(letter) {
 
 // alreadyGuessed fucntion to check if the input letter from the user is already guessed
 function alreadyGuessed(guess) {
-  for (let i = 0; i < alreadyGuessedArray.length; i++) {
-    if (guess == alreadyGuessedArray.length[i]) {
-      return true;
+  let chk = false;
+  for (let k = 0; k < alreadyGuessedArray.length; k++) {
+    if (guess == alreadyGuessedArray[k]) {
+      console.log("already guess");
+      chk = true;
+      break;
     } else {
-      console.log("ashraf");
-      return false;
+      chk = false;
     }
   }
+
+  return chk;
 }
 
 function check(guess) {
   let guessedLetter = guess.key.toLowerCase();
   if (checkLetter(guess)) {
     let founded = false;
+
     if (alreadyGuessed(guessedLetter)) {
-      alert("already guessed letter");
+      alert("Already guessed letter");
     } else {
+      alreadyGuessedArray.push(guessedLetter);
+      console.log(alreadyGuessedArray);
+      console.log(alreadyGuessedArray.length);
       for (let i = 0; i < letters.length; i++) {
         if (guessedLetter == letters[i]) {
-          console.log("good job");
           founded = true;
-          alreadyGuessedArray.push(guessedLetter);
-          console.log(guessedLetter);
-          console.log(alreadyGuessedArray);
           break;
         }
-        founded = false;
       }
     }
   } else {
@@ -56,7 +59,6 @@ function check(guess) {
 
 // function updateHTML() {}
 document.addEventListener("keydown", function (event) {
-  // console.log("game start");
   guess = event;
   check(guess);
 });
