9e8ac9967b03311a1d781ed401ed41b32c30bc55
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-02 20:26:39 +0200

    fixing some bugs in the check function

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,12 @@
 const words = ["web"];
 // const words = ["software", "web", "laravel", "javascript", "nabiha"];
-const attempts = 6;
+let attempts = 6;
 const randomNum = Math.floor(Math.random() * words.length);
 const word = words[randomNum];
-
+const answer = document.querySelector("letter");
 let letters = word.split("");
 let alreadyGuessedArray = [];
-let correctLetter;
+let correctLetters = letters;
 let guess;
 
 //  checkLetter function to check if the pressed key is a letter using ASCII codes
@@ -23,14 +23,10 @@ function alreadyGuessed(guess) {
   let chk = false;
   for (let k = 0; k < alreadyGuessedArray.length; k++) {
     if (guess == alreadyGuessedArray[k]) {
-      console.log("already guess");
       chk = true;
       break;
-    } else {
-      chk = false;
     }
   }
-
   return chk;
 }
 
@@ -38,18 +34,22 @@ function check(guess) {
   let guessedLetter = guess.key.toLowerCase();
   if (checkLetter(guess)) {
     let founded = false;
-
     if (alreadyGuessed(guessedLetter)) {
       alert("Already guessed letter");
     } else {
       alreadyGuessedArray.push(guessedLetter);
-      console.log(alreadyGuessedArray);
-      console.log(alreadyGuessedArray.length);
       for (let i = 0; i < letters.length; i++) {
         if (guessedLetter == letters[i]) {
           founded = true;
           break;
         }
+      }
+      if (founded) {
+        correctLetters = correctLetters.filter(
+          (item) => item !== guessedLetter
+        );
+      } else {
+        attempts -= 1;
       }
     }
   } else {
@@ -58,7 +58,8 @@ function check(guess) {
 }
 
 // function updateHTML() {}
+
 document.addEventListener("keydown", function (event) {
   guess = event;
-  check(guess);
+    check(guess);
 });
